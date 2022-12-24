147d62fddd35d8aa996a1b0a2bef57214c34562a
janselmi <janselmi@auction.com>
2022-12-23 21:23:51 -0300

    Print board before running the simulator

--- simulation1.py
+++ simulation1.py
@@ -14,9 +14,11 @@ player2_cards = minion_pool.get_random_minion_set(3)
 player1 = Player("Fedex", player1_cards)
 player2 = Player("Javi", player2_cards)
 
+Game(player1,player2,verbose=True).print_board()
+
 # Step 3: Run the multigame simulator
-#sim = MultigameSimulator(10000, player1, player2)
-#sim.run_all_games()
+sim = MultigameSimulator(10000, player1, player2)
+sim.run_all_games()
 
 # OPTIONAL: If you want to only play one game, and see the output, uncomment the next line.
-Game(player1,player2,verbose=True).start_game()
+# Game(player1,player2,verbose=True).start_game()
