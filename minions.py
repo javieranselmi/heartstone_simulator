2d4cfc604cdd41343ccbad5a41baec99aef8071b
janselmi <janselmi@auction.com>
2022-12-28 00:35:21 -0300

    JSONificating minions

--- minions.py
+++ minions.py
@@ -1,35 +1,45 @@
 from card import Card
 from deathrattle.summon import Summon
 import random
+import json
 
 class Minions:
+
+    def card_from_json(self, card_json):
+
+        attack = card_json["attack"]
+        hit_points = card_json["hit_points"]
+        level = card_json["level"]
+        name = card_json["name"]
+
+        taunt = card_json["taunt"] if "taunt" in card_json else False
+        divine_shield = card_json["divine_shield"] if "divine_shield" in card_json else False
+        poisonous = card_json["poisonous"] if "poisonous" in card_json else False
+
+        if "deathrattle" in card_json:
+            deathrattle_type = card_json["deathrattle"]["type"]
+            if deathrattle_type == "summon":
+                card = card_json["deathrattle"]["card"]
+                deathrattle = Summon(self.card_from_json(card))
+            else:
+                raise Exception("Deathrattle not implemented")
+        else:
+            deathrattle = None
+
+        return Card(name, level, attack, hit_points, taunt, divine_shield, poisonous, deathrattle)
+
+
     def __init__(self):
-        self.minion_pool = [
-            Card("allycat",  1, 1, deathrattle=Summon(Card("tabycat",1,1))),  #bc
-            Card("swabbie", 2, 2),
-            Card("chroma", 1, 4),
-            Card("imprisoner", 2, 2, taunt=True),
-            Card("micromummy", 1, 2, deathrattle=Summon(Card("micromummy", 1, 1))),  #rb
-            Card("mini-myrmidon", 3, 3),
-            Card("picky eater", 2, 3),  #?
-            Card("pupbot", 2, 1, divine_shield=True),
-            Card("geomancer", 3, 1),
-            Card("red whelp", 2, 2),
-            Card("anomally", 1, 4),
-            Card("rockpool", 2, 3),
-            Card("scallywag", 3, 1, deathrattle=Summon(Card("pirate",1,1))),
-            Card("hyena", 2, 2),
-            Card("sellemental", 2, 2),
-            Card("shell collector", 3, 1),
-            Card("sun beaconer", 1, 2),
-            Card("swamp striker", 1, 4),
-            Card("wrath weaver", 1, 3),
-            Card("Tavern Tipper", 2, 2),
-        ]
+
+        f = open('minions.json')
+        minion_data = json.loads(f.read())
+        self.minion_pool = [ self.card_from_json(card_json) for card_json in minion_data['cards'] ]
+        f.close()
 
 
-    def get_random_minion_list(self, minions_count):
-        return [random.choice(self.minion_pool) for m in range(minions_count)]
+    def get_random_minion_list(self, minions_count, max_level=None):
+        minion_pool = [m for m in self.minion_pool if m.level <= max_level] if max_level else self.minion_pool
+        return [random.choice(minion_pool) for m in range(minions_count)]
 
     def get_card_list_by_name(self, name_array):
         card_list = []
@@ -39,3 +49,4 @@ class Minions:
                     card_list.append(minion)
                     break
         return card_list
+        
